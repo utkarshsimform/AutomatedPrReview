9cc265ba9758341e1b9be5d31d25b66f6d50730f
Utkarsh Prajapati <utkarsh.p@simformsolutions.com>
2025-05-27 12:05:47 +0530

    Test commit

--- AutomatedPRReview/Demo2.cs
+++ AutomatedPRReview/Demo2.cs
@@ -1,9 +1,7 @@
 using System;
 
-/// <summary>
-/// Another well-formed demo class for PR review.
-/// </summary>
-public class Demo2
+// TODO: Refactor this class name to PascalCase
+public class demo2 // Not PascalCase
 {
     /// <summary>
     /// Returns the sum of two integers.
@@ -12,4 +10,28 @@ public class Demo2
     {
         return a + b + d;
     }
+
+    // TODO: Add XML documentation
+    public void addNumbers() // Not PascalCase
+    {
+        String MyString = "Hello"; // Not camelCase, and String instead of string
+        int unusedVariable = 0; // Unused variable
+        for (int i = 0; i < 10; i++) // Missing curly braces
+            Console.WriteLine(i); // Console.WriteLine usage
+        if (MyString == null) // Missing curly braces
+            MyString.ToString(); // Possible null dereference
+        try
+        {
+            // ...existing code...
+        }
+        catch (Exception)
+        {
+            // No logging or rethrow
+        }
+        // select * from Users where password = '1234'; // Possible SQL injection, hardcoded password
+        // This is commented-out code;
+    }
+
+    // Extra blank line below
+
 }
